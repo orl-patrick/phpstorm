d0cf122ad59d38b25258e16d2cd437dbca8509e7
orl-patrick <patrickfaucheux@alicepro.fr>
2017-02-27 15:14:06 +0100

    initial commit

---
+++ eleves.php
@@ -0,0 +1,16 @@
+<?php
+
+$students = [
+    "Emmanuel"  => 42,
+    "Thierry"   => 51,
+    "Pascal"    => 45,
+    "Eric"      => 48,
+    "Nicolas"   => 19,
+];
+
+var_dump($students);
+
+
+
+
+?>
